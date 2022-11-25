bae403715a424c1334de12a3e7a528fb737d7e96
Hamza Bouziane <hbouziane@smb.sqli.com>
2022-11-25 16:57:56 +0100

    refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -46,19 +46,19 @@ class GildedRose {
     }
 
     if (item.sellIn < 0) {
-      if (!item.name.equals("Aged Brie")) {
-        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-          if (item.quality > 0) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-              item.quality = item.quality - 1;
-            }
-          }
-        } else {
-          item.quality = 0;
-        }
-      } else {
+      if (item.name.equals("Aged Brie")) {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
+        }
+      } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        item.quality = 0;
+      }
+      else if(item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
+      }
+      else {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
       }
     }
