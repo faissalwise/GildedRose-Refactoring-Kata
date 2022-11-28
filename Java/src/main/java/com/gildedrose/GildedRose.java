958461c2911fe8f60a0734825be54ce5b36525ca
ylechqar <adm-azr-youssef.lechqar@chanel.com>
2022-11-28 16:23:57 +0100

    if refacto
    all name if are at the first level

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,10 +14,21 @@ class GildedRose {
   }
 
   private void updateItemQuality(Item item) {
+
+    // first block
     if (item.name.equals("Aged Brie")) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
       }
+
+      item.sellIn = item.sellIn - 1;
+
+      if (item.sellIn < 0) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+
     } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
@@ -34,31 +45,19 @@ class GildedRose {
           }
         }
       }
+      item.sellIn = item.sellIn - 1;
+      if (item.sellIn < 0) {
+        item.quality = 0;
+      }
     } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
     } else {
       if (item.quality > 0) {
         item.quality = item.quality - 1;
       }
-    }
-
-    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
       item.sellIn = item.sellIn - 1;
-    }
-
-    if (item.sellIn < 0) {
-      if (item.name.equals("Aged Brie")) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        item.quality = 0;
-      }
-      else if(item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
-      }
-      else {
+      if (item.sellIn < 0) {
         if (item.quality > 0) {
-            item.quality = item.quality - 1;
+          item.quality = item.quality - 1;
         }
       }
     }
